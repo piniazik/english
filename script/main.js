1195967ddfb349d30d7a151fdb547fbb10cfe980
Aleksei <52882331+piniazik@users.noreply.github.com>
2022-03-14 09:34:05 +0300

    feat: add new folder and files

---
+++ script/main.js
@@ -0,0 +1,78 @@
+//preloader
+$(window).on('load', function(){
+  $('.preloader').delay(2000).fadeOut('slow',);
+});
+
+//sound play
+$('.down__btn').on('click', function breakDownWord() {
+  let breakDown = document.querySelector('.break__down');
+  if (breakDown.paused) {
+    breakDown.play();
+    breakDown.setAttribute('loop', 'loop');
+    /*breakDown = setTimeout(breakDownWord, 8000);*/
+  } else {
+    breakDown.pause();
+    breakDown.removeAttribute('loop', 'loop');
+  }
+});
+
+$('.in__btn').on('click', function breakInWord() {
+  let breakIn = document.querySelector('.break__in');
+  if (breakIn.paused) {
+    breakIn.play();
+    breakIn.setAttribute('loop', 'loop');
+    /*breakIn = setTimeout(breakInWord, 8000);*/
+  } else {
+    breakIn.pause();
+    breakIn.removeAttribute('loop', 'loop');
+  }
+});
+
+$('.into__btn').on('click', function breakIntoWord() {
+  let breakInto = document.querySelector('.break__into');
+  if (breakInto.paused) {
+    breakInto.play();
+    breakInto.setAttribute('loop', 'loop');
+    /*breakInto = setTimeout(breakIntoWord, 8000);*/
+  } else {
+    breakInto.pause();
+    breakInto.removeAttribute('loop', 'loop');
+  }
+});
+
+$('.off__btn').on('click', function breakOffWord() {
+  let breakOff = document.querySelector('.break__off');
+  if (breakOff.paused) {
+    breakOff.play();
+    breakOff.setAttribute('loop', 'loop');
+    /*breakOff = setTimeout(breakOffWord, 8000);*/
+  } else {
+    breakOff.pause();
+    breakOff.removeAttribute('loop', 'loop');
+  }
+});
+
+$('.out__btn').on('click', function breakOutWord() {
+  let breakOut = document.querySelector('.break__out');
+  if (breakOut.paused) {
+    breakOut.play();
+    breakOut.setAttribute('loop', 'loop');
+    /*breakOut = setTimeout(breakOutWord, 8000);*/
+  } else {
+    breakOut.pause();
+    breakOut.removeAttribute('loop', 'loop');
+  }
+});
+ 
+$('.trough__btn').on('click', function breakTroughWord() {
+  let breakTrough = document.querySelector('.break__trough');
+  if (breakTrough.paused) {
+    breakTrough.play();
+    breakTrough.setAttribute('loop', 'loop');
+    /*breakTrough = setTimeout(breakTroughWord, 8000);*/
+  } else {
+    breakTrough.pause();
+    breakTrough.removeAttribute('loop', 'loop');
+  }
+});
+ 
